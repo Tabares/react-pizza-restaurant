a5482ccc5b0a601f3ed3bd7a35aff4f396118e3c
Jose Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2023-06-08 01:36:51 -0600

    Add new format update approaches

--- src/components/PizzaItem.tsx
+++ src/components/PizzaItem.tsx
@@ -1,111 +1,121 @@
-import React from 'react';
-import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import axios from "axios";
+import {Link, useParams} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
 
-import {InfoBlock} from './InfoBlock';
+import {InfoBlock} from "./InfoBlock";
 
-import { categoriesDictionary } from './Categories';
-import { typeDictionary } from './PizzaBlock';
+import {categoriesDictionary} from "./Categories";
+import {typeDictionary} from "./PizzaBlock";
 
-import { addItem } from '../redux/cart/slice';
-import { RootState } from '../redux/store';
-import { TPizza } from '../redux/pizza/types';
+import {addItem} from "../redux/cart/slice";
+import {RootState} from "../redux/store";
+import {TPizza} from "../redux/pizza/types";
+import {TCartItem} from "../redux/cart/types";
 
-export const PizzaItem:React.FC = () => {
-    const { id } = useParams();
+export const PizzaItem: React.FC = () => {
+  const {id} = useParams();
+  const [pizza, setPizza] = React.useState<TPizza>();
+  const [activeType, setActiveType] = React.useState<number>(0);
+  const [activeSizeIndex, setActiveSizeIndex] = React.useState<number>(0);
 
-    const [pizza, setPizza] = React.useState<TPizza>();
-
-    const [activeType, setActiveType] = React.useState<number>(0);
-    const [activeSizeIndex, setActiveSizeIndex] = React.useState<number>(0);
-
-    const items = useSelector((state: RootState) => state.cart.items);
-
-    const dispatch = useDispatch();
-
-    let count: number = 0;
-
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].id === id) {
-            count += items[i].count;
-        }
+  const items = useSelector((state: RootState) => state.cart.items);
+   const count = items.reduce((count: number, val: TCartItem) => {
+    if (val.id === id) {
+      count += val.count;
     }
 
-    React.useEffect(() => {
-        axios.get(`https://62b7554a691dcea2733d6cff.mockapi.io/items/${id}`).then((res) => {
-            setPizza(res.data);
-        });
-    }, []);
+    return count;
+  }, 0);
 
-    return !pizza ? (
-        <InfoBlock title="Loading..." description="The e-pizza hasn't finished baking yet :>" />
-    ) : (
-        <div className="pizza-item">
-            <div className="pizza-item__left-side">
-                <img src={pizza.imageUrl} alt="pizza" />
-            </div>
-            <div className="pizza-item__right-side">
-                <h1>{pizza.title}</h1>
-                <p className="pizza-item__subtitle">Type: {categoriesDictionary[pizza.category]}</p>
-                <div className="pizza-item__selector">
-                    <ul>
-                        {pizza.types.map((typeIndex, index) => (
-                            <li
-                                key={index}
-                                className={activeType === index ? 'active' : ''}
-                                onClick={() => setActiveType(index)}>
-                                {typeDictionary[typeIndex]}
-                            </li>
-                        ))}
-                    </ul>
+  const dispatch = useDispatch();
 
-                    <ul>
-                        {pizza.sizes.map((size, index) => (
-                            <li
-                                key={index}
-                                className={activeSizeIndex === index ? 'active' : ''}
-                                onClick={() => setActiveSizeIndex(index)}>
-                                {size} cm.
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <p className="pizza-item__description">
-                    Rating <span></span> {pizza.rating} / 10
-                </p>
+ 
 
-                <p className="pizza-item__description">
-                    Quantity <span></span> {count}
-                </p>
+  React.useEffect(() => {
+    axios
+      .get(`https://62b7554a691dcea2733d6cff.mockapi.io/items/${id}`)
+      .then((res) => {
+        setPizza(res.data);
+      });
+  }, []);
 
-                <p className="pizza-item__description">
-                    Price <span></span> $ {pizza.price}
-                </p>
+  return !pizza ? (
+    <InfoBlock
+      title="Loading..."
+      description="The e-pizza hasn't finished baking yet :>"
+    />
+  ) : (
+    <div className="pizza-item">
+      <div className="pizza-item__left-side">
+        <img src={pizza.imageUrl} alt="pizza" />
+      </div>
+      <div className="pizza-item__right-side">
+        <h1>{pizza.title}</h1>
+        <p className="pizza-item__subtitle">
+          Type: {categoriesDictionary[pizza.category]}
+        </p>
+        <div className="pizza-item__selector">
+          <ul>
+            {pizza.types.map((typeIndex, index) => (
+              <li
+                key={index}
+                className={activeType === index ? "active" : ""}
+                onClick={() => setActiveType(index)}
+              >
+                {typeDictionary[typeIndex]}
+              </li>
+            ))}
+          </ul>
 
-                <div className="pizza-item__buttonGroup">
-                    <Link to="/">
-                        <button className="pizza-item__button">Back</button>
-                    </Link>
-                    <button
-                        className="pizza-item__button"
-                        onClick={() =>
-                            dispatch(
-                                addItem({
-                                    id,
-                                    title: pizza.title,
-                                    image: pizza.imageUrl,
-                                    type: typeDictionary[activeType],
-                                    size: pizza.sizes[activeSizeIndex],
-                                    price: pizza.price,
-                                    count: 1,
-                                }),
-                            )
-                        }>
-                        Add
-                    </button>
-                </div>
-            </div>
+          <ul>
+            {pizza.sizes.map((size, index) => (
+              <li
+                key={index}
+                className={activeSizeIndex === index ? "active" : ""}
+                onClick={() => setActiveSizeIndex(index)}
+              >
+                {size} cm.
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+        <p className="pizza-item__description">
+          Rating <span></span> {pizza.rating} / 10
+        </p>
+
+        <p className="pizza-item__description">
+          Quantity <span></span> {count}
+        </p>
+
+        <p className="pizza-item__description">
+          Price <span></span> $ {pizza.price}
+        </p>
+
+        <div className="pizza-item__buttonGroup">
+          <Link to="/">
+            <button className="pizza-item__button">Back</button>
+          </Link>
+          <button
+            className="pizza-item__button"
+            onClick={() =>
+              dispatch(
+                addItem({
+                  id,
+                  title: pizza.title,
+                  image: pizza.imageUrl,
+                  type: typeDictionary[activeType],
+                  size: pizza.sizes[activeSizeIndex],
+                  price: pizza.price,
+                  count: 1,
+                })
+              )
+            }
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
